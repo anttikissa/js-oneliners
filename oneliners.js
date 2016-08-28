083d2ca81dd1e011d81644deb4f7ab4741760346
Antti Sykäri <antti.sykari@gmail.com>
2016-08-28 18:17:30 +0300

    Create oneliners.js

---
+++ oneliners.js
@@ -0,0 +1,13 @@
+// Get an element. $('body > header')
+let $ = (selector) => document.querySelector(selector);
+
+// Get a NodeList of elements. for (p in $$('p')) { p.textContent = 'hello' }
+let $$ = (selector) => document.querySelectorAll(selector);
+
+// Create element, initializing it with attributes
+// let myDiv = el('div')
+// let myParagraph = el('p', { class: 'green', textContent: 'hello' })
+let el = (name, attrs) => Object.assign(document.createElement(name), attrs);
+
+// Calculate a checksum of a string as a floating point number between -1 and 1; empty string maps to 0
+let checksum = (string) => string.split('').map(x => x.charCodeAt(0)).reduce((x, y) => Math.sin(123 * x + y), 0)
