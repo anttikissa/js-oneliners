d1d102816aad6f8416bef61e656d84bc340bab64
Antti Sykäri <antti.sykari@gmail.com>
2016-10-22 18:11:51 +0300

    Add a oneliner that watches for changes in required files

--- oneliners.js
+++ oneliners.js
@@ -10,4 +10,9 @@ let $$ = (selector) => document.querySelectorAll(selector);
 let el = (name, attrs) => Object.assign(document.createElement(name), attrs);
 
 // Calculate a checksum of a string as a floating point number between -1 and 1; empty string maps to 0
-let checksum = (string) => string.split('').map(x => x.charCodeAt(0)).reduce((x, y) => Math.sin(123 * x + y), 0)
+let checksum = (string) => string.split('').map(x => x.charCodeAt(0)).reduce((x, y) => Math.sin(x + y), 0);
+
+// Quit node.js process when any of the required modules changes.
+// To restart process automatically, run your server in a loop like:
+//   while node main.js; do :; done
+Object.keys(require.cache).forEach(key => require('fs').watch(key, () => process.exit(0)));
