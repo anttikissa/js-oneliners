f04ecd18d939f20aadf793169de10e6ef06cb7fa
Antti Kissaniemi <antti.sykari@gmail.com>
2017-09-25 17:33:01 +0300

    Add pad2, nth

--- oneliners.js
+++ oneliners.js
@@ -9,8 +9,8 @@ const $$ = (selector) => document.querySelectorAll(selector);
 // let myParagraph = el('p', { class: 'green', textContent: 'hello' })
 const el = (name, attrs) => Object.assign(document.createElement(name), attrs);
 
-// Calculate a checksum of a string as a floating point number between -1 and 1; empty string maps to 0
-const checksum = (string) => string.split('').map(x => x.charCodeAt(0)).reduce((x, y) => Math.sin(x + y), 0);
+// Calculate a (probably a bad) checksum of a string as a floating point number between -1 and 1; empty string maps to 0
+const checksum = (string) => string.split('').map(x => x.charCodeAt(0)).reduce((x, y) => Math.sin(x + y + x * y), 0);
 
 // Quit node.js process when any of the required modules changes.
 // To restart process automatically, run your server in a loop like:
@@ -22,12 +22,29 @@ const autoexit = () => Object.keys(require.cache).forEach(key => require('fs').w
 // Redirecting stderr is left as an exercise for the reader.
 const npmRun = () => require('child_process').exec(require('./package.json').scripts[process.argv[2]]).stdout.pipe(process.stdout);
 
-// Generate a UUID v4 too cleverly (source: https://gist.github.com/jed/982883)
+// Generate a UUID v4 too cleverly
+// Source: https://gist.github.com/jed/982883
 const uuid = (a) => a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, uuid);
 
 // Calculate entropy of a message (such as a password)
-const entropy = (s) => [...new Set(s)].map(c => s.split('').filter(d => d === c).length).reduce((e, freq) => e + freq * Math.log2(s.length / freq), 0);
+// Source:
+// https://gist.github.com/ppseprus/afab8500dec6394c401734cb6922d220
+// Adapted to return the entropy of the whole message.
+const entropy = (s) => [...new Set(s)].map(c => s.match(new RegExp(c, 'g')).length).reduce((e, freq) => e + freq * Math.log2(s.length / freq), 0);
+
+// The infamous pad2
+// Source: https://medium.com/@p_arithmetic/a-collection-of-my-6-favorite-javascript-one-liners-7c80a4b731f8
+const pad2 = (s) => ('0' + s).substr(-2);
+
+// Format 1 => '1st', 2 => '2nd' etc.
+// Source:
+// https://medium.com/@p_arithmetic/a-collection-of-my-6-favorite-javascript-one-liners-7c80a4b731f8
+// Shaved some characters by using .exec() instead of .match()
+const nth = (o) => o + (['st', 'nd', 'rd'][/1?\d\b/.exec(o) - 1] || 'th');
 
 module.exports = {
-	entropy
+	checksum,
+	entropy,
+	nth,
+	pad2
 };
--- oneliners.test.js
+++ oneliners.test.js
@@ -1,9 +1,57 @@
-const { entropy } = require('./oneliners');
+const { entropy, nth, pad2 } = require('./oneliners');
 
 test('entropy', () => {
 	expect(entropy('abbcccddddaa')).toBe(23.509775004326936);
 	expect(entropy('1223334444')).toBe(18.464393446710154);
 	expect(entropy('')).toBe(0);
-	expect(entropy('1234')).toBe(8);
+	expect(entropy('1234')).toBe(2 * 4);
+	expect(entropy('1234abcd')).toBe(3 * 8);
+	expect(entropy('1234abcd5678XYZW')).toBe(4 * 16);
 	expect(entropy('correcthorsebatterystaple')).toBe(84.09640474436812);
+});
+
+test('nth', () => {
+	expect(nth(1)).toBe('1st');
+	expect(nth(2)).toBe('2nd');
+	expect(nth(3)).toBe('3rd');
+	expect(nth(4)).toBe('4th');
+	expect(nth(5)).toBe('5th');
+	expect(nth(6)).toBe('6th');
+	expect(nth(7)).toBe('7th');
+	expect(nth(10)).toBe('10th');
+	expect(nth(11)).toBe('11th');
+	expect(nth(12)).toBe('12th');
+	expect(nth(13)).toBe('13th');
+	expect(nth(20)).toBe('20th');
+	expect(nth(21)).toBe('21st');
+	expect(nth(22)).toBe('22nd');
+	expect(nth(23)).toBe('23rd');
+	expect(nth(24)).toBe('24th');
+	expect(nth(28)).toBe('28th');
+	expect(nth(30)).toBe('30th');
+	expect(nth(31)).toBe('31st');
+	expect(nth(32)).toBe('32nd');
+	expect(nth(33)).toBe('33rd');
+	expect(nth(34)).toBe('34th');
+	expect(nth(38)).toBe('38th');
+	expect(nth(100)).toBe('100th');
+	expect(nth(101)).toBe('101st');
+	expect(nth(102)).toBe('102nd');
+	expect(nth(103)).toBe('103rd');
+	expect(nth(104)).toBe('104th');
+	expect(nth(110)).toBe('110th');
+	expect(nth(111)).toBe('111th');
+	expect(nth(112)).toBe('112th');
+	expect(nth(113)).toBe('113th');
+});
+
+test('pad2', () => {
+	expect(pad2(0)).toBe('00');
+	expect(pad2(1)).toBe('01');
+	expect(pad2(10)).toBe('10');
+	expect(pad2(11)).toBe('11');
+	expect(pad2(99)).toBe('99');
+	// Whoops!
+	expect(pad2(110)).toBe('10');
+	expect(pad2(111)).toBe('11');
 });
